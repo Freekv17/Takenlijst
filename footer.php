c927688cc79b3d243d1d80bcb5f1dec5434d9e82
bwalting <babita.alting@hotmail.com>
2025-03-11 08:31:39 +0100

    mapstructure + css

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Takenlijst DeveloperLand</title>
+    <link rel="stylesheet" href="public/style.css" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <footer>
+        <div class="wrapper">
+            <a href="index.php">
+                <img src="/img/logo-big-v2.png" alt="logoV2" id="logoV2">
+            </a>
+            <div class="extraInfo">
+                <div class="extraInfoLeft">
+                    <h2>Lorem ipsum</h2>
+                    <p>dolor sit amet consectetur</p>
+                    <p>adipiscing elit nulla vulputate</p>
+                    <p>neque sed</p>
+                </div>
+                <div class="extraInfoRight">
+                    <h2>Lorem ipsum</h2>
+                    <p>dolor sit amet consectetur</p>
+                    <p>adipiscing elit nulla vulputate</p>
+                    <p>neque sed</p>
+                </div>
+
+            </div>
+
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Takenlijst DeveloperLand</title>
+    <link rel="stylesheet" href="public/style.css" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php require_once('header.php'); ?>
+    <main>
+        <div class="wrapper">
+            <img src="/img/logo-big-fill-only.png" alt="logoFill" id="logoFill">
+        </div>
+    </main>
+    <?php require_once('footer.php'); ?>
+
+</body>
+
+
+
+</html>
