fa693cc99a95b399a9372e57eacc2b4643a099b5
bwalting <babita.alting@hotmail.com>
2025-03-11 10:06:08 +0100

    hoiii

--- tasks/create.php
+++ tasks/create.php
@@ -7,16 +7,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Developerland Takenlijst</title>
+    <title>Takenlijst DeveloperLand</title>
+    <link rel="stylesheet" href="<?php echo $base_url; ?>public/style.css" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
-    <?php require_once("../header"); ?>
+    <?php require_once("../header.php"); ?>
 
     <main></main>
 
 
+    <?php require_once('../footer.php'); ?>
 
 </body>
 
