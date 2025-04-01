7b1c018a773a41913fd57a9bb44a394c66fd3a89
Candle <fvoermans11@gmail.com>
2025-04-01 09:22:15 +0200

    Create edit.php

---
+++ tasks/edit.php
@@ -0,0 +1,78 @@
+<?php require_once("../config/conn.php"); ?>
+<?php require_once("../config/config.php"); ?>
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once '../public/resources/views/components/head.php'; ?>
+
+<body>
+
+    <?php
+    if (!isset($_SESSION['user_id'])) {
+        $msg = 'Je moet eerst inloggen';
+        header("Location: ../login.php?msg=$msg");
+        exit;
+    }
+    ?>
+
+
+    <?php require_once "../public/resources/views/components/header.php"; ?>
+
+    <main>
+
+
+        <?php
+
+        $query = "SELECT * FROM Takenlijst WHERE user_id = :user_id";
+        $statement = $conn->prepare($query);
+        $statement->execute([
+            ":user_id" => $_SESSION['user_id'],
+        ]);
+
+        $table = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+
+        <div class="wrapper">
+            <form action="../backend/tasksController.php" method="POST">
+                <input type="hidden" name="action" value="edit">
+
+                <div class="form-group">
+                    <label for="titel">Titel: </label>
+                    <input type="text" name="title" id="title" value="<?php $table['title'] ?>">
+                </div>
+                <div class="form-group">
+                    <label for="beschrijving">Beschrijving: </label>
+                    <input type="text" name="beschrijving" id="beschrijving">
+                </div>
+                <div class="form-group">
+                    <label for="deadline">Deadline:</label>
+                    <input type="date" name="deadline" id="deadline">
+                </div>
+                <div class="form-group">
+                    <label for="afdeling">Afdeling: </label>
+                    <select name="department" id="department">
+                        <option value="">- Kies een afdeling -</option>
+                        <option value="personeel">personeel</option>
+                        <option value="horeca">horeca</option>
+                        <option value="techniek">techniek</option>
+                        <option value="inkoop">inkoop</option>
+                        <option value="klantenservice">klantenservice</option>
+                        <option value="groen">groen</option>
+                        <option value="attracties">attracties</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Maak taak!">
+                </div>
+            </form>
+        </div>
+    </main>
+
+
+    <?php require_once '../public/resources/views/components/footer.php'; ?>
+
+</body>
+
+</html>
